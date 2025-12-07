543a1dc6d14b31b182ecda01304f739e46b59c0a
anticroom <anticroom01@gmail.com>
2025-12-07 14:55:38 -0500

    fix and tweaks

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -44,7 +44,7 @@ export default {
                 <div class="player-container">
                     <div class="player">
                         <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
-                        <h3>{{ entry.total }} Points Total</h3>
+                        <h3>{{ entry.total }} <span class="small-h3">Points Total</h3>
                         <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
                         <table class="table">
                             <tr v-for="score in entry.verified">
